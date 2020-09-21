25c1611c10f7fef458823a62123fe3480285efa8
Andrey Yakimenko <59168235+and-yakim@users.noreply.github.com>
2020-09-21 17:46:53 +0300

    Update New_molecules.py

--- New_molecules.py
+++ New_molecules.py
@@ -1,6 +1,27 @@
-class MoleculesSimulator:
+from tkinter import *
+
+
+class Quadrant:
     def __init__(self):
         pass
+
+
+class Molecules:
+    def __init__(self):
+        pass
+
+
+class MoleculesSimulator:
+    def __init__(self):
+        self.width, self.height = 800, 500
+        self.root = Tk()
+        self.root.geometry('+300+100')
+
+        self.field = Canvas(self.root, width=self.width,
+                            height=self.height, bg='silver')
+        self.field.pack()
+
+        self.root.mainloop()
 
 
 def main():
